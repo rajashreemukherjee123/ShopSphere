627b8a06af2374e627841651371d3465dc9aaa11
Tomar Namrajashreemukherjee123 <rajashreemukherjee28@gmail.com>
2026-08-23 01:01:58 +0530

    add dialog in cart remove button

--- FrontEnd_E-commerce_website_v002/src/components/cart/CartItem.jsx
+++ FrontEnd_E-commerce_website_v002/src/components/cart/CartItem.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Typography, Box, styled, Button } from '@mui/material'
+import React, { useState } from 'react'
+import { Typography, Box, styled, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+
 
 import { addEllipsis } from '../../utils/common-utils'
 
 import  ButtonGroup from './ButtonGroup';
 
 import { removeFromCartAction } from '../../redux/actions/cartAction';
+import {addToWishList} from "../../redux/actions/wishListAction"
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 
@@ -34,21 +36,63 @@ const Remove = styled(Button)`
 
 
 const CartItem = ({item}) => {
-    if (!item.productId) return null;
+    if (!item?.productId){
+        return null;
+    } 
 
     const product = item.productId; 
 
-    if (!product) return null; // Safety check
-
     const dispatch = useDispatch();
 
-    const removeItemFromCart = (id) =>{
-        console.log("Removing product with ID:", id);
-        dispatch(removeFromCartAction(id));
-        toast.info("Product removed from Cart");
+    const [openRemoveDialog, setOpenRemoveDialog] = useState(false);
+
+    // Open Dialog
+    const handleRemoveClick = ()=>{
+        setOpenRemoveDialog(true);
+    }
+
+    // Close Dialog
+    const handleCloseDialog = ()=>{
+        setOpenRemoveDialog(false);
+    }
+
+    if (!product) return null; // Safety check
+
+    
+    // Remove Dialog
+    const removeItemFromCart = async() =>{
+        try{
+            await dispatch(removeFromCartAction(product._id));
+
+            setOpenRemoveDialog(false);
+
+            toast.info("Product removed from cart");
+
+        }catch(err){
+            toast.error("Unable to remove product from cart");
+        }
+    }
+
+
+    // Move oriduct to wishlist
+    const moveToWishlist = async()=>{
+        try{
+            await dispatch(addToWishList(product._id));
+
+            await dispatch(removeFromCartAction(product._id));
+        
+            setOpenRemoveDialog(false);
+
+            toast.success("Product moved to wishlist ❤️");
+        
+        }catch(err){
+            toast.error("Unable to move product to wishlist");
+        }
     }
 
   return (
+<>
+    
     <Component>
       <LeftComponent>
             <img src={product.url} alt="product" style={{ height: 110, width: 110, objectFit: 'contain' }}/>
@@ -62,9 +106,71 @@ const CartItem = ({item}) => {
                         <Box component="span" style={{ color: "#878787" }}><strike>₹{product.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
                         <Box component="span" style={{ color: "#388E3C" }}>{product.price.discount} off</Box>
             </Typography>
-            <Remove onClick={()=>removeItemFromCart(item.productId._id)}>Remove</Remove>
+            <Remove onClick={handleRemoveClick}>Remove</Remove>
       </Box>
     </Component>
+
+
+    {/* ------------------ Remove Confirmation Dialog ------------ */}
+    <Dialog 
+        open={openRemoveDialog}
+        onClose={handleCloseDialog}
+        maxWidth="xs"
+        fullWidth
+    >
+         <DialogTitle sx={{fontWeight: 600}}>
+            Move item to wishlist?
+         </DialogTitle>
+
+
+         <DialogContent>
+            <Typography color='text.secondary'>
+                Find it easily and buy when you're ready 
+                
+        or, 
+        remove it completely from your cart 
+            </Typography>
+         </DialogContent>
+
+
+         <DialogActions>
+            {/* Move to Wishlist */}
+            <Button variant='outlined'
+                onClick={moveToWishlist}
+                sx={{
+                    textTransform: "none",
+                    borderColor: "#512886",
+                    color: "#512886",
+
+                    "&:hover": {
+                        borderColor: "#85399c",
+                        backgroundColor:
+                            "rgba(109, 70, 161, 0.04)"
+                    }
+                }} 
+            >
+                Move to Wishlist
+            </Button>
+
+                {/* Remove */}
+            <Button variant='contained'
+                color='error'
+                onClick={removeItemFromCart}
+                sx={{ textTransform: "none" }}
+            >
+                Remove item
+            </Button>
+
+                {/* Cancle */}
+            <Button onClick={handleCloseDialog}
+                sx={{ textTransform: "none" }}
+            >
+                Cancel
+            </Button>
+         </DialogActions>
+    </Dialog>
+    
+</>
   )
 }
 
